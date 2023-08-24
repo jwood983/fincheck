2ef8e6450741ec70a86040794bd2aeb015cdd349
Josh Wood <j.d.wood83@gmail.com>
2023-08-23 22:16:52 -0400

    Update extract.py
    Adding two additional functions to improve functionality; minor whitespace

--- src/fincheck/extract.py
+++ src/fincheck/extract.py
@@ -1,6 +1,8 @@
+""" This is the primary API for this package; most users need to access the methods in this file. """
 from .validate import is_cusip, is_isin, is_aba, is_sedol
 from .utils import find_and_validate
-from typing import *
+from typing import List, Dict, 
+
 
 def get_cusips(s: str) -> List[str]:
     """
@@ -69,19 +71,18 @@ def get_sedols(s: str) -> List[str]:
     pattern = r"((?<=[^\w])|(?<=^))([0-9BCDFGHJKLMNPQRSTVWXYZ]{6}[0-9])(?=[^\w]|$)"
     return find_and_validate(s, pattern, validation_fn=is_sedol)
 
+
 def find_securities(s: str, include: List = ["CUSIP", "ISIN", "SEDOL"]) -> Dict:
     allowed_types = {
         "CUSIP": get_cusips, 
         "ISIN": get_isins, 
         "SEDOL": get_sedols
         }
-    include = [x.upper() for x in include] #ensure upper
-    include = [x for x in include if x in allowed_types] #ensure types are valid
+    # ensure that the allowed security measures are correct
+    include = [x.upper() for x in include if x.upper() in allowed_types]
     assert len(include) > 0, "Must include at least one of the following: CUSIP, ISIN, or SEDOL"
-    res = {}
-    for t in include:
-        res[t] = allowed_types[t](s)
-    return res
+    return {t: allowed_types[t](s) for t in include}
+
 
 def get_abas(s: str) -> List[str]:
     """
@@ -110,3 +111,54 @@ def get_abas(s: str) -> List[str]:
     return find_and_validate(s, pattern, validation_fn=is_aba)
 
 
+def process_substrings(s: str, include: List[str] = ["CUSIP", "ISIN", "SEDOL"]) -> Dict:
+    """
+    This method allows users to check that the substrings of the input string match the various
+    types of identifiers. 
+
+    Args:
+        s (str): The string to process to see if a slice of it is a valid cusip, isin or sedol string.
+        include (list): The name of the validation checks. Should be one of CUSIP, ISIN or SEDOL
+
+    Returns:
+        dict. The key-value dictionary with each check paired with the set of matches for the check.
+    """
+    def _range(name: str) -> int:
+        """ get the length of the expected strings. """
+        return {"CUSIP": 8, "ISIN": 12, "SEDOL": 12}.get(name)
+        
+    allowed_types = Dict(
+        CUSIP=get_cusips,
+        ISIN=get_isins,
+        SEDOL=get_sedols
+    )
+    
+    # ensure that the allowed security measures are correct
+    include = [x.upper() for x in include if x.upper() in allowed_types]
+    assert len(include) > 0, "Must include at least one of the following: CUSIP, ISIN, or SEDOL"
+    result = dict()
+    
+    # process each slice of the string
+    for id_type, caller in allowed_types.items():
+        for idx in _range(id_type):
+            result[id_type] = result.get(id_type, []).extend(caller(s[idx:idx+_range(id_type)]))
+    return result
+    
+    
+def process_list(l: List[str], include: List[str] = ["CUSIP", "ISIN", "SEDOL"]) -> Dict:
+    """
+    This method allows users to check that the substrings of the input set of strings match the 
+    various types of identifiers. 
+
+    Args:
+        l (list): The list of strings to process to see if a slice of it is a valid cusip, isin or sedol string.
+        include (list): The name of the validation checks. Should be one of CUSIP, ISIN or SEDOL
+
+    Returns:
+        dict. The key-value dictionary with each check paired with the set of matches for the check.
+    """
+    p = process_substrings(l[0], include)
+    for ell in l[1:]:
+        p.update(process_substrings(ell, include))
+    return p
+    
